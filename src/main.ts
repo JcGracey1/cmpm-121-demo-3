b4908f70a8b6e86801716ec9fd033be48d7502fa
Jacqueline <jacqueline.gracey@gmail.com>
2024-11-03 12:34:25 -0800

    L16 add alert

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,18 @@
-// todo
+import "./style.css";
+
+const app: HTMLDivElement = document.querySelector("#app")!;
+
+const gameName = "D3";
+document.title = gameName;
+
+const header = document.createElement("h1");
+header.innerHTML = gameName;
+app.append(header);
+
+const testButton = document.createElement("button");
+testButton.innerHTML = "Click";
+app.append(testButton);
+
+testButton.addEventListener("click", () => {
+    alert("You clicked the button!");
+})
